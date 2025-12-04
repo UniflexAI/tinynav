acd0116e91781ba5d3e79dcdbade08591d9754fb
Xiaole Fang <xiaole.fang@deepmirror.com>
2025-12-05 07:37:04 +0800

    fix: Fix the foxglove_bridge argument format in run_streamer.sh (#174)
    * Update run_streamer.sh to enhance foxglove_bridge command with new argument syntax for port and topic whitelist
    
    * minor fix
    
    ---------
    
    Co-authored-by: xiaole-dm <xiaole.fang@deepmirrror.com>

--- scripts/run_streamer.sh
+++ scripts/run_streamer.sh
@@ -9,6 +9,6 @@ set -e
 tmux new-session \; \
   split-window -h \; \
   split-window -v \; \
-  select-pane -t 0 \; send-keys 'ros2 run foxglove_bridge foxglove_bridge --port 8765 --topic-whitelist /camera/camera/infra1/image_rect_raw_repub,/camera/camera/infra1/camera_info' C-m \; \
+  select-pane -t 0 \; send-keys 'ros2 run foxglove_bridge foxglove_bridge --ros-args -p port:=8765 -p topic_whitelist:="[/camera/camera/infra1/image_rect_raw_repub,/camera/camera/infra1/camera_info]"' C-m \; \
   select-pane -t 1 \; send-keys 'ros2 run image_transport republish raw foxglove --ros-args --remap in:=/camera/camera/infra1/image_rect_raw -r out/foxglove:=/camera/camera/infra1/image_rect_raw_repub -p out.foxglove.qmax:=60 -p out.foxglove.bit_rate:=8000000' C-m \; \
   select-pane -t 2 \; send-keys 'ros2 topic bw /camera/camera/infra1/image_rect_raw_repub' C-m 
