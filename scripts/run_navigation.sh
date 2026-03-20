30a11ff05397a8a02de96ae04199257c378d7209
junlinp <junlinp@deepmirror.com>
2026-03-20 21:36:42 +0800

    chore(run_navigation): remove lekiwi_control pane

--- scripts/run_navigation.sh
+++ scripts/run_navigation.sh
@@ -8,7 +8,7 @@ tmux new-session \; \
   select-pane -t 0 \; split-window -v \; \
   select-pane -t 0 \; send-keys 'uv run python /tinynav/tinynav/core/perception_node.py' C-m \; \
   select-pane -t 1 \; send-keys 'uv run python /tinynav/tinynav/core/planning_node.py' C-m \; \
-  select-pane -t 2 \; send-keys 'uv run python /tinynav/tinynav/platforms/lekiwi_control.py' C-m \; \
+  select-pane -t 2 \; send-keys 'bash' C-m \; \
   select-pane -t 3 \; send-keys "uv run python /tinynav/tinynav/core/map_node.py --tinynav_map_path $map_path" C-m \; \
   select-pane -t 4 \; send-keys "uv run python /tinynav/platforms/cmd_vel_control.py" C-m \; \
   select-pane -t 5 \; send-keys 'ros2 run rviz2 rviz2 -d /tinynav/docs/vis.rviz' C-m \; \
