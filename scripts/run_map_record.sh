58e5c6d124ba69de213c30817813f8dc732d484a
YANG Zhenfei <yangzhenfei0@gmail.com>
2026-04-02 19:23:51 +0800

    scripts: hardcode docker script paths for map record

--- scripts/run_map_record.sh
+++ scripts/run_map_record.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-
 tmux new-session \; \
   split-window -v \; \
-  select-pane -t 0 \; send-keys "bash '${script_dir}/run_realsense_sensor.sh'" C-m \; \
-  select-pane -t 1 \; send-keys "sleep 3; bash '${script_dir}/run_rosbag_record.sh'" C-m
+  select-pane -t 0 \; send-keys "bash /tinynav/scripts/run_realsense_sensor.sh" C-m \; \
+  select-pane -t 1 \; send-keys "sleep 3; bash /tinynav/scripts/run_rosbag_record.sh" C-m
