21c65bfee41225745deb5509b15cd75a517d29e1
YANG Zhenfei <yangzhenfei0@gmail.com>
2026-04-03 18:11:11 +0800

    Open rqt with preview perspective

--- scripts/run_sensors_preview.sh
+++ scripts/run_sensors_preview.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 tmux new-session \; \
   split-window -v \; \
   select-pane -t 0 \; send-keys "bash /tinynav/scripts/run_realsense_sensor.sh" C-m \; \
-  select-pane -t 1 \; send-keys "rqt" C-m
+  select-pane -t 1 \; send-keys "rqt --perspective-file /tinynav/docs/preview.perspective" C-m
